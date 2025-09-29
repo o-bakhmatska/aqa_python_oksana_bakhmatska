688ddb17da6d4f5c552e4183c046fc1ac57d02c5
o-bakhmatska <bahmatskaya.o@gmail.com>
2025-09-29 14:14:47 +0300

    Added homework 5.1 and 5.2

---
+++ lesson_5/homework_5.2.py
@@ -0,0 +1,34 @@
+# Given list of tuples (name, surname, age, profession, City location)
+# 1 - Add your new record o the beginning of the given list
+# 2 - In modified list swap elements with indexes 1 and 5 (1<->5). Print result
+# 3 - check that all people in modified list with records indexes 6, 10, 13
+#   have age >=30. Print condition check result
+
+people_records = [
+  ('John', 'Doe', 28, 'Engineer', 'New York'),
+  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+
+new_record = ('Alex', 'Black', 32, 'AQA', 'Kyiv')
+people_records.insert(0, new_record)
+print("Task 1.", people_records)
+
+people_records[1], people_records[5] = people_records[5], people_records[1]
+print("Task 2.", people_records)
+
+checked_indexes = [6, 10, 13]
+check_condition = all(people_records[i][2] >= 30 for i in checked_indexes)
+print("Task 3. Чи усі з віком >=30 на позиціях 6, 10, 13 ? ->", check_condition)
