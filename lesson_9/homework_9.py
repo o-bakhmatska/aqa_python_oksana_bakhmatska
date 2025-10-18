bb94dc59b092ec6b2a7a96c5296129e24f6a4152
o-bakhmatska <bahmatskaya.o@gmail.com>
2025-10-19 00:41:12 +0300

    Fix issues in Homework 9

--- lesson_9/homework_9.py
+++ lesson_9/homework_9.py
@@ -2,6 +2,7 @@ class Rhombus:
     def __init__(self, side_a, angle_a):
         self.side_a = side_a
         self.angle_a = angle_a
+        self.angle_b = 180 - angle_a
 
     def __setattr__(self, key, value):
         if key == "side_a":
@@ -11,14 +12,17 @@ class Rhombus:
                 super().__setattr__(key, value)
 
         elif key == "angle_a":
-            if not (0 < value < 180):
+            if  not (0 < value < 180):
                 print("НЕ МОЖНА змінити кут. Кут 'α' повинен бути не включно між 0 та 180° ")
             else:
                 super().__setattr__(key, value)
                 super().__setattr__("angle_b", 180 - value)
 
-        else:
-            super().__setattr__(key, value)
+        elif key == "angle_b":
+            if not (self.angle_a + value == 180):
+                print("НЕ МОЖНА змінити кут. Сума кутів α і β повинна дорівнювати 180°.")
+            else:
+                super().__setattr__(key, value)
 
     def display_info(self):
         return f'Ромб: Сторона a: {self.side_a}, Кут α: {self.angle_a}°, Кут β: {self.angle_b}°'
@@ -26,15 +30,18 @@ class Rhombus:
 r1= Rhombus(25, 179)
 print(r1.display_info())
 
-print("\nЗмінюю кут 'α' на валідне значення 150°:")
-r1.angle_a = 150
+print("\nЗмінюю кут 'β' на НЕ валідне значення:")
+r1.angle_b = 180
 print(r1.display_info())
 
-print("\nЗмінюю кут 'α' на НЕ валідне значення 181°:")
+print("\nЗмінюю кут 'α' на НЕ валідне значення:")
 r1.angle_a = 181
 print(r1.display_info())
 
-print("\nЗмінюю сторону 'а' на НЕ валідне значення -15:")
-r1.side_a = -15
+print("\nЗмінюю кут 'α' на валідне значення:")
+r1.angle_a = 150
 print(r1.display_info())
 
+print("\nЗмінюю сторону 'а' на НЕ валідне значення:")
+r1.side_a = -15
+print(r1.display_info())
