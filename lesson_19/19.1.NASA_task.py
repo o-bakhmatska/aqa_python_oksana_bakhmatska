78ba0600752fb237956ebac5543bce8aec4133a5
o-bakhmatska <bahmatskaya.o@gmail.com>
2025-11-22 23:38:46 +0200

    Updated Homework 19.1 with functions

--- lesson_19/19.1.NASA_task.py
+++ lesson_19/19.1.NASA_task.py
@@ -2,37 +2,54 @@ import requests
 import random
 import os
 
+def get_data(url):
+    response = requests.get(url)
+    return response.json()
 
-base_url = "https://images-api.nasa.gov/search?q=mars&media_type=image"
-response = requests.get(base_url)
-data = response.json()
+def extract_image_urls(data):
+    images_urls = []
 
-images_urls = []
+    items = data.get("collection", {}).get("items", [])
+    for item in items:
+        links = item.get("links", [])
+        for link in links:
+            href = link.get("href")
+            if href and href.endswith((".jpg", ".jpeg", ".png")):
+                images_urls.append(href)
 
-items = data.get("collection", {}).get("items", [])
+    return images_urls
 
-for item in items:
-    links = item.get("links", [])
-    for link in links:
-        href = link.get("href")
-        if href and href.endswith((".jpg", ".jpeg", ".png")):
-            images_urls.append(href)
+def choose_random_images(image_urls, count=3):
+    return random.sample(image_urls, count)
 
-print(f"Found {len(images_urls)} images")
+def download_and_save_images(image_urls, folder="NASA_images"):
+    os.makedirs(folder, exist_ok=True)
 
-images_quantity = 3
-images_selected = random.sample(images_urls, images_quantity)
+    for i, image_url in enumerate(image_urls, start=1):
+        img_data = requests.get(image_url).content
+        filename = os.path.join(folder, f"nasa_image_{i}.jpg")
 
-folder = "NASA_images"
-os.makedirs(folder, exist_ok=True)
+        with open(filename, "wb") as f:
+            f.write(img_data)
 
-for i, image_url in enumerate(images_selected, start=1):
-    img_data = requests.get(image_url).content
-    filename = os.path.join(folder, f"nasa_image_{i}.jpg")
-    with open(filename, "wb") as f:
-        f.write(img_data)
+        print(f"Image saved: {filename} {image_url}")
 
-    print(f"Image saved: {filename} {image_url}")
+
+def main():
+    base_url = "https://images-api.nasa.gov/search?q=mars&media_type=image"
+
+    data = get_data(base_url)
+
+    images_urls = extract_image_urls(data)
+    print(f"Found {len(images_urls)} images")
+
+    selected = choose_random_images(images_urls, 3)
+
+    download_and_save_images(selected)
+
+
+if __name__ == "__main__":
+    main()
 
 
 
